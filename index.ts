2b9e2d52bd4047a413a836075307f8ad0b21d559
Mariusz Rajczakowski (mariocoski) <mariusz@proximie.com>
2021-05-31 17:24:15 +0100

    feat: add express app listening in default port

--- index.ts
+++ index.ts
@@ -1,1 +1,14 @@
+import express from "express";
+const app = express();
+
 export const DEFAULT_PORT_NUMBER: number = 3000;
+
+app.use("*", (_, res) => {
+  res.send({
+    message: "OK",
+  });
+});
+
+app.listen(DEFAULT_PORT_NUMBER, () => {
+  console.log(`Server is listening on a port: ${DEFAULT_PORT_NUMBER}`);
+});
