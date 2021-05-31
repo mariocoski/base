21fa634543327f8602c86fb594976d1158e90be6
Mariusz Rajczakowski (mariocoski) <mariusz@proximie.com>
2021-05-31 16:11:45 +0100

    change pre-commit

--- .huskyrc.js
+++ .huskyrc.js
@@ -1,7 +1,7 @@
 
 module.exports = {
   hooks: {
-    "pre-commit": "echo 'Running pre-commit checks... (skip using --no-verify)' && sh ./scripts/pre-commit-message.sh",
+    "pre-commit": "npm run test",
   }
 };
 
